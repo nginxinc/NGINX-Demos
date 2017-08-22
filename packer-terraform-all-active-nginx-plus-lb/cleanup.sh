565476a974e31a01d21a1248fda835ff98c34d43
Alessandro Fael Garcia <alessfg@hotmail.com>
2017-08-22 15:00:17 -0700

    Add support for TF 0.10.2

--- packer-terraform-all-active-nginx-plus-lb/cleanup.sh
+++ packer-terraform-all-active-nginx-plus-lb/cleanup.sh
@@ -1,1 +1,1 @@
-cd terraform && yes yes | terraform destroy && gcloud compute images delete nginx-plus-lb-image nginx-oss-app-1-image nginx-oss-app-2-image --quiet
+cd terraform && yes yes | terraform destroy && rm -rf .terraform && gcloud compute images delete nginx-plus-lb-image nginx-oss-app-1-image nginx-oss-app-2-image --quiet
