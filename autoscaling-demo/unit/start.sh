e84a1cda57b7186e5dfe854ac7281a01a9ea4cee
tienidurodad <rick.nelson@att.net>
2019-01-25 10:33:13 -0800

    Update start.sh
    Update to work with newer version of Unit and add the HTTP status codes to error messages.

--- autoscaling-demo/unit/start.sh
+++ autoscaling-demo/unit/start.sh
@@ -2,18 +2,18 @@
 
 unitd --modules /usr/lib/unit/modules/ --log /var/log/unit.log
 
-i=`curl --unix-socket /run/control.unit.sock -s -o /dev/null -w "%{http_code}" localhost`
+i=`curl --unix-socket /run/control.unit.sock -s -o /dev/null -w "%{http_code}" localhost/config`
 if [ "$i" != "200" ]; then
-    echo "Unit failed to start"
+    echo "Unit failed to start: Status $1"
     exit 1
 fi
 
 echo "Started Unit"
 
-i=`curl --unix-socket /run/control.unit.sock -s -o /dev/null -w "%{http_code}" -X PUT -d @/srv/app/app.config localhost`
+i=`curl --unix-socket /run/control.unit.sock -s -o /dev/null -w "%{http_code}" -X PUT -d @/srv/app/app.config localhost/config`
 
 if [ "$i" != "200" ]; then
-    echo "Unit configuration failed"
+    echo "Unit configuration failed: Status $i"
     exit 1
 fi
 
@@ -22,8 +22,10 @@ echo "Unit configured"
 while [ "$i" = "200" ];
 do
     sleep 30;
-    i=`curl --unix-socket /run/control.unit.sock -s -o /dev/null -w "%{http_code}" localhost`
+    i=`curl --unix-socket /run/control.unit.sock -s -o /dev/null -w "%{http_code}" localhost/config`
+    if [ "$i" != "200" ]; then
+        echo "Error from Unit API: Status $i"
+    fi
 done
 
 echo "The Unit start script has exited"
-
