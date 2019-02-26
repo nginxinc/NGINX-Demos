da1f9d7ce689e735cbda3ee0f2cf1482028a5113
Alessandro Fael Garcia <alessfg@hotmail.com>
2019-02-26 14:48:06 +0100

    Final AWS NLB updates

--- aws-nlb-ha-asg/packer/setup-instance.sh
+++ aws-nlb-ha-asg/packer/setup-instance.sh
@@ -4,14 +4,14 @@ sudo yum install nginx-asg-sync-0.2-1.amzn1.x86_64.rpm -y
 sudo rm nginx-asg-sync-0.2-1.amzn1.x86_64.rpm
 
 cat > /tmp/default.conf <<EOF
-upstream app-one {
-  zone app-one 64k;
-  state /var/lib/nginx/state/app-one.conf;
+upstream app_one {
+  zone app_one 64k;
+  state /var/lib/nginx/state/app_one.conf;
 }
 
-upstream app-two {
-  zone app-two 64k;
-  state /var/lib/nginx/state/app-two.conf;
+upstream app_two {
+  zone app_two 64k;
+  state /var/lib/nginx/state/app_two.conf;
 }
 
 server {
@@ -23,16 +23,16 @@ server {
     root /usr/share/nginx/html;
   }
 
-  location /app-one {
+  location /app_one {
     proxy_set_header Host \$host;
     proxy_set_header X-Forwarded-For \$remote_addr;
-    proxy_pass http://app-one/;
+    proxy_pass http://app_one/;
   }
 
-  location /app-two {
+  location /app_two {
     proxy_set_header Host \$host;
     proxy_set_header X-Forwarded-For \$remote_addr;
-    proxy_pass http://app-two/;
+    proxy_pass http://app_two/;
   }
 }
 
@@ -57,11 +57,11 @@ region: us-west-1
 api_endpoint: http://127.0.0.1:8080/api
 sync_interval_in_seconds: 5
 upstreams:
-  - name: app-one
+  - name: app_one
     autoscaling_group: ngx-oss-1-autoscaling
     port: 80
     kind: http
-  - name: app-two
+  - name: app_two
     autoscaling_group: ngx-oss-2-autoscaling
     port: 80
     kind: http
--- aws-nlb-ha-asg/setup.sh
+++ aws-nlb-ha-asg/setup.sh
@@ -1,8 +1,7 @@
 cd terraform \
-&& yes yes | terraform destroy \
-&& rm -rf .terraform \
 && terraform init \
 && cd ../packer \
 && packer build -force ./packer.json \
 && cd ../terraform \
+&& yes yes | terraform destroy \
 && yes yes | terraform apply
