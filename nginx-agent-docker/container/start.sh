5740e6bcad5a8f26abb9207258838fec9c5721b7
Fabrizio Fiorucci <fiorucci@oasi.asti.it>
2023-03-21 15:00:30 +0100

    NGINX App Protect updates

--- nginx-agent-docker/container/start.sh
+++ nginx-agent-docker/container/start.sh
@@ -15,7 +15,15 @@ fi
 
 if [[ "$NAP_WAF" == "true" ]]; then
    PARM="${PARM} --nginx-app-protect-report-interval 15s --nap-monitoring-collector-buffer-size 50000 --nap-monitoring-processor-buffer-size 50000 --nap-monitoring-syslog-ip 127.0.0.1 --nap-monitoring-syslog-port 514"
+   /opt/app_protect/bin/bd_agent &
    /usr/share/ts/bin/bd-socket-plugin tmm_count 4 proc_cpuinfo_cpu_mhz 2000000 total_xml_memory 471859200 total_umu_max_size 3129344 sys_max_account_id 1024 no_static_config &
+
+   while ([ ! -e /opt/app_protect/pipe/app_protect_plugin_socket ] || [ ! -e /opt/app_protect/pipe/ts_agent_pipe ])
+   do
+     sleep 1
+   done
+
+   chown nginx:nginx /opt/app_protect/pipe/*
 fi
 
 if [[ "$NAP_WAF_PRECOMPILED_POLICIES" == "true" ]]; then
