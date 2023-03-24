aff1c8363f3851b34c5ad3f7286816297054acab
Fabrizio Fiorucci <fiorucci@oasi.asti.it>
2023-03-24 11:16:24 +0100

    Fixed for NGINX Instance Manager 2.9.0

--- nginx-agent-docker/container/start.sh
+++ nginx-agent-docker/container/start.sh
@@ -13,10 +13,11 @@ if [[ ! -z "$NIM_TAGS" ]]; then
    PARM="${PARM} --tags $NIM_TAGS"
 fi
 
+
 if [[ "$NAP_WAF" == "true" ]]; then
    PARM="${PARM} --nginx-app-protect-report-interval 15s --nap-monitoring-collector-buffer-size 50000 --nap-monitoring-processor-buffer-size 50000 --nap-monitoring-syslog-ip 127.0.0.1 --nap-monitoring-syslog-port 514"
-   /opt/app_protect/bin/bd_agent &
-   /usr/share/ts/bin/bd-socket-plugin tmm_count 4 proc_cpuinfo_cpu_mhz 2000000 total_xml_memory 471859200 total_umu_max_size 3129344 sys_max_account_id 1024 no_static_config &
+   su - nginx -s /bin/bash -c "/opt/app_protect/bin/bd_agent &"
+   su - nginx -s /bin/bash -c "/usr/share/ts/bin/bd-socket-plugin tmm_count 4 proc_cpuinfo_cpu_mhz 2000000 total_xml_memory 471859200 total_umu_max_size 3129344 sys_max_account_id 1024 no_static_config &"
 
    while ([ ! -e /opt/app_protect/pipe/app_protect_plugin_socket ] || [ ! -e /opt/app_protect/pipe/ts_agent_pipe ])
    do
@@ -34,4 +35,4 @@ if [[ "$ACM_DEVPORTAL" == "true" ]]; then
    nginx-devportal server &
 fi
 
-nginx-agent $PARM
+sg nginx-agent "/usr/bin/nginx-agent $PARM"
