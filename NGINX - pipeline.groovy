91b792a50592bbe49d338e9750fdae562ab3f9db
Onur Ozcelik <onurozcelik_@hotmail.com>
2023-09-07 03:03:42 +0100

    Update NGINX - pipeline.groovy

--- NGINX - pipeline.groovy
+++ NGINX - pipeline.groovy
@@ -1,17 +1,35 @@
 pipeline {
     agent any
 
+    environment {
+        DOCKERHUB_CREDENTIALS = credentials('docker-hub-credentials')
+    }
+
     stages {
-        stage('Checkout') {
+        stage('Clone Repository') {
             steps {
-                checkout scm
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[url: 'https://github.com/OnurOzcelikSE/NGINX-Demos.git']]])
+                echo 'Repository cloned'
             }
         }
-
         stage('Build Docker Image') {
             steps {
                 script {
-                    def dockerImage = docker.build('OnurOzcelikSE/NGINX-Demos', './nginx-hello')
+                    def registryCredentials = [
+                        usernamePassword(
+                            credentialsId: 'DOCKERHUB_CREDENTIALS.id',
+                            usernameVariable: 'DOCKERHUB_USERNAME',
+                            passwordVariable: 'DOCKERHUB_PASSWORD'
+                        )
+                    ]
+                    echo 'Logged in to DockerHub'
+
+                    docker.withRegistry('https://hub.docker.com', registryCredentials) {
+                        def imageName = "onurozcelikse/nginx-hello"
+                        def dockerImage = docker.build(imageName)
+                        dockerImage.push()
+                    echo 'Docker image builded'
+                    }
                 }
             }
         }
@@ -19,10 +37,14 @@ pipeline {
         stage('Push Docker Image') {
             steps {
                 script {
-                    docker.withRegistry('https://index.docker.io/v1/', 'onurozcelikse') {
-                        dockerImage.push()
-                    }
+                    docker.withRegistry('https://index.docker.io/v1/', 'onurozcelikse') 
+                echo 'Docker Image Pushed'
                 }
+            }
+        }
+        stage('Analyze Docker Image') {
+            steps {
+                sh 'dive --ci onurozcelikse/nginx-hello --exit-code'
             }
         }
     }
